d3017f7f6d1a5eb56ba7aa9ed73ea8a6c730af2c
Maxime SAUVAGE <msauvage.cda@FORMATION.LOCAL>
2025-11-20 13:30:41 +0400

    feat: ajout de la classe Admin qui étend Utilisateur

---
+++ src/Entity/Admin.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AdminRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AdminRepository::class)]
+#[ORM\Table(name: '`admin`')]
+
+class Admin extends Utilisateur
+{
+    #[ORM\Column(length: 50)]
+    private ?string $statut = null;
+
+    public function getStatut(): ?string
+    {
+        return $this->statut;
+    }
+
+    public function setStatut(string $statut): static
+    {
+        $this->statut = $statut;
+        return $this;
+    }
+    // Les propriétés et méthodes sont héritées de Utilisateur
+}
